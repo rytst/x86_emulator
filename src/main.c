af6bb26394de162393e43153fb28686a9f2002d3
rytst <r.saito0111@gmail.com>
2024-10-26 13:05:44 +0900

    register struct

--- src/main.c
+++ src/main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define REGISTERS_COUNT 1000000;
+enum register {
+    EAX,
+    ECX,
+    EDX,
+    EBX,
+    ESP,
+    EBP,
+    ESI,
+    EDI,
+    REGISTERS_COUNT
+};
+
+char* registers_name[] = {
+    "EAX",
+    "ECX",
+    "EDX",
+    "EBX",
+    "ESP",
+    "EBP",
+    "ESI",
+    "EDI"
+};
 
 typedef struct {
     uint32_t registers[REGISTERS_COUNT];
     uint32_t eflags;
     uint8_t* memory;
+
+    /*
+    * Program counter
+    */
     uint32_t eip;
 } emulator;
 
-int main(void) {
+int main(int argc, char* argv[]) {
     return 0;
 }
