aa74bb9dd2f57e17dac5220ad0c4ab5a2546daa3
rytst <r.saito0111@gmail.com>
2024-10-26 15:45:04 +0900

    add Makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+TARGET := x86_emulator
+BUILD_DIR := ./build
+SRC_DIR := ./src
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 enum register {
@@ -26,15 +27,41 @@ char* registers_name[] = {
 
 typedef struct {
     uint32_t registers[REGISTERS_COUNT];
+
+    /*
+     * EFLAGS register
+     */
     uint32_t eflags;
+
     uint8_t* memory;
 
     /*
-    * Program counter
+    * Program counter (Instruction Pointer)
     */
     uint32_t eip;
 } emulator;
 
+static emulator* init_emu(size_t mem_size, uint32_t eip, uint32_t esp);
+static void      destroy_emu(emulator* emu);
+
 int main(int argc, char* argv[]) {
     return 0;
+}
+
+static emulator* init_emu(size_t mem_size, uint32_t eip, uint32_t esp) {
+
+    /*
+     * Generate emulator
+     */
+    emulator* emu = (emulator*)malloc(sizeof(emulator));
+
+
+    /*
+     * Initialize emulator
+     */
+    emu->memory        = (uint8_t*)calloc(sizeof(uint8_t), mem_size);
+    emu->eip           = eip;
+    emu->register[ESP] = esp;
+
+    return emu;
 }
